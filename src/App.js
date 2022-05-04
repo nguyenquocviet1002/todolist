415e6d4b2982b4187497996ff9ef4975f73ee566
Quốc Việt <quocviet@funtap.vn>
2022-05-04 17:57:00 +0700

    update todo.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ const data = [
   { id: "todo-1", name: "Sleep", completed: false },
   { id: "todo-2", name: "Repeat", completed: false }
 ];
-const ele = data.map((item, index) => {
-  return (
-    <Todo name={index.name} completed={index.completed} id={index.id}/>
-  )
-})
-console.log(ele)
+
 function App() {
   return (
     <div className="bg-white w-1/3 h-full mx-auto mt-5 shadow-md p-10">
       <h1 className="text-3xl text-center font-semibold">Todo List</h1>
-      <ul>
-        {/* <Todo name={data.name} completed={data.completed} id={data.id}/> */}
-      </ul>
+        <Todo data={data}/>
     </div>
   );
 }
